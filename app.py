237c82b3616cf1a47eeb2fa103ba07168cb6cdfb
Benjamin Parkka <Bparkka@comcast.net>
2019-11-26 23:23:06 -0500

    Problem set

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/venues")
+def venues():
+    #Open csv file in Unicode
+    file = open("entertainment-licenses.csv", "r", encoding="utf-8")
+    reader = csv.reader(file)
+    next(reader) # Skip the first line
+    venues = list(reader) # Create a list
+    file.close()
+    return render_template("venues.html", venues=venues)
+
+@app.route("/search", methods=["POST"])
+def search():
+    # Implement YOUR CODE HERE
+    return "hello, world"
